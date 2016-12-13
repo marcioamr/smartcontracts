116997164fe52bba3cb112f4337cf731dcea80cd
marcioamr <marcioamr@users.noreply.github.com>
2016-12-13 14:42:19 -0200

    Update SimpleSample.java

--- labs/lab03/SimpleSample/src/main/java/example/SimpleSample.java
+++ labs/lab03/SimpleSample/src/main/java/example/SimpleSample.java
@@ -150,7 +150,7 @@ public class SimpleSample extends ChaincodeBase {
 
 	@Override
 	public String getChaincodeID() {
-		return "Percival";
+		return "MarcioAlexandre";
 	}
 
 	public static void main(String[] args) throws Exception {
